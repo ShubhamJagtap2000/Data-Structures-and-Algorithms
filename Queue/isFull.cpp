73b5ad144253d8004208dcc69ab3f897b9e0a10d
ShubhamJagtap2000 <63872951+ShubhamJagtap2000@users.noreply.github.com>
2021-07-23 00:16:39 +0530

    Queue isFull() operation

---
+++ Queue/isFull.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int arr[4];
+  int front = -1;
+  int rear = -1;
+  
+  bool isEmpty()
+  {
+    if(front == -1 && rear == -1)
+      return true
+    else
+      return false
+   }
+}
+  
