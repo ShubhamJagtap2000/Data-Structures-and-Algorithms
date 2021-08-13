330ceebab38f613939d894437c58fc0388969c30
ShubhamJagtap2000 <63872951+ShubhamJagtap2000@users.noreply.github.com>
2021-08-14 00:40:19 +0530

    reversing a linked list using recursion in C/C++

--- Linked List/Reversal/reverse-a-linked-list-using-recursion.cpp
+++ Linked List/Reversal/reverse-a-linked-list-using-recursion.cpp
@@ -1,1 +1,25 @@
+//#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+//using namespace std;
 
+struct Node
+{
+  int data;
+  struct Node* next;
+};
+
+struct Node* head;
+
+void Reverse(struct Node* p)
+{
+  if(p -> next == NULL)
+  {
+    head = p;
+    return;
+  }
+  Reverse(p -> next);
+  struct Node* q = p -> next;
+  q -> next = p;
+  p -> next = NULL;
+}
