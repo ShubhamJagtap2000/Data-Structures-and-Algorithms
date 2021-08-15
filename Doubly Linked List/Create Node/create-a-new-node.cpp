87ccbf15f729e1fd6fd604f37fc8a5a857b5332e
ShubhamJagtap2000 <63872951+ShubhamJagtap2000@users.noreply.github.com>
2021-08-15 23:38:55 +0530

    create a node for doubly linked list in C/C++

--- Doubly Linked List/Create Node/create-a-new-node.cpp
+++ Doubly Linked List/Create Node/create-a-new-node.cpp
@@ -1,1 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+//#include<iostream>
+//using namespace std;
 
+struct Node
+{
+  int data;
+  struct Node* next;
+  struct Node* prev;
+};
+
+struct Node* head;  //global variable - pointer to head node
+
+int main()
+{
+  return 0;
+}
+  
