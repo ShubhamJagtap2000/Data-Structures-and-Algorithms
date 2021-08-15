a96ec530ea6911c5f7fc37b358a9ce5ca431e474
ShubhamJagtap2000 <63872951+ShubhamJagtap2000@users.noreply.github.com>
2021-08-15 23:55:54 +0530

    Insert a node at head of a doubly linked list using C/C++

--- Doubly Linked List/Insertion/insert-a-node-at-head.cpp
+++ Doubly Linked List/Insertion/insert-a-node-at-head.cpp
@@ -1,1 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+//#include<iostream>
+//using namespace std;
 
+struct Node
+{
+  int data;
+  struct Node* next;
+  struct Node* prev;
+};
+
+struct Node* head;  //global variable - pointer to head node
+
+struct Node* GetNewNode(int x)
+{
+  //local variable
+  //will be cleared from memory when function call will finish, so we will not use it.
+  /*struct Node myNode;
+  myNode.data = x;
+  myNode.prev = NULL;
+  myNode.next = NULL;
+  return &newNode*/
+  
+  struct Node* newNode = (struct Node*)malloc(sizeof(struct Node)); //OR
+  temp -> data = x;
+  temp -> prev = NULL;
+  temp -> next = NULL;
+  return newNode;
+}
+
+void InsertAtHead(int x)
+{
+  struct Node* newNode = GetNewNode(x);   //different local variable
+  if(head == NULL)
+  {
+    head = newNode;
+    return;
+  }
+  head -> prev = newNode;
+  newNode -> next = head;
+  head = newNode;
+}
+
+int main()
+{
+  InsertAtHead(2);
+  InsertAtHead(4);
+}
