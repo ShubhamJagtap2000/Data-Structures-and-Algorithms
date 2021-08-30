0dff0d47c2d429b3ec3954d4239f2732abdff796
ShubhamJagtap2000 <63872951+ShubhamJagtap2000@users.noreply.github.com>
2021-08-31 00:11:26 +0530

    Queue - Circular Array implementation in C++

--- Queue/ArrayBasedQueue.cpp
+++ Queue/ArrayBasedQueue.cpp
@@ -1,3 +1,4 @@
+/* Queue - Circular Array implementation in C++*/
 #include<iostream>
 using namespace std;
 
@@ -11,39 +12,42 @@ class Queue
     int rear;
   
   public:
+  // Constructor - set front and rear as -1. 
+	// We are assuming that for an empty Queue, both front and rear will be -1.
     Queue()
     {
       front = -1;
       rear = -1;
     }
   
-  bool isEmpty()
-  {
-    return (front == -1 && rear == -1); 
-  }
+    bool isEmpty()
+    {
+      return (front == -1 && rear == -1); 
+    }
   
-  bool isFull()
-  {
-    return (rear+1)%MAX_SIZE == front ? true : false;
-  }
+    bool isFull()
+    {
+      return (rear + 1) % MAX_SIZE == front ? true : false;
+    }
 
-  void Enqueue(int x)
-  {
-    cout << "Enqueuing " << x << "\n";
-    if isFull()
+    void Enqueue(int x)
     {
-      cout << "Error: Queue is Full" << endl;
-      return;
+      cout << "Enqueuing " << x << "\n";
+      if isFull()
+      {
+        cout << "Error: Queue is Full" << endl;
+        return;
+      }
+      if isEmpty()
+      {
+        front = rear = 0;
+      }
+      else
+      {
+        rear = (rear + 1) % MAX_SIZE;
+      }
+      A[rear] = x;
     }
-    if isEmpty()
-    {
-      front = rear = 0;
-    }
-    else
-    {
-      rear = (rear + 1) % MAX_SIZE;
-    }
-    A[rear] = x;
 
     void Dequeue()
     {
@@ -67,9 +71,33 @@ class Queue
     {
       if(front == -1)
       {
-        cout << "
+        cout << "Error: cannot return front from empty queue\n";
+        return -1;
+      }
+      return A[front];
+    }
     
-
+    //This function is only to test the code. 
+	  //This is not a standard function for Queue implementation.
+    void Print()
+    {
+      int count = (rear + MAX_SIZE - front) % MAX_SIZE + 1;
+      cout << "Queue ";
+      for(int i = 0; i < count; i++)
+      {
+        int index = (front + i) % MAX_SIZE;
+        cout << A[index] << " ";
+      }
+      cout << endl << endl;
+    }
+};
+    
 int main()
 {
+   Queue Q; // creating an instance of Queue Q
+   Q.Enqueue(2);  Q.Print();  
+   Q.Enqueue(4);  Q.Print();  
+   Q.Enqueue(6);  Q.Print();
+   Q.Dequeue();	  Q.Print();
+   Q.Enqueue(8);  Q.Print();
 }
